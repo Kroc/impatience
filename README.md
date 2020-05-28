5255ad1771a4aa173fb7fa96c0066f5a0d513bf0
Kroc Camen <kroc@camendesign.com>
2020-05-28 13:22:28 +0100

    Get something on screen:
    Draws up a simple menu mockup.
    
    Adds a couple of tilesets (2 & 4-wide cards, and 2.5-wide cards).
    
    RGDS' `rgbfix` tool is used to convert PNG to 2bpp format.

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 In development. Mockup:
 
 ![Mockup screenshot of a Klondike Solitaire game](screenshot.png)
+
+## Goals:
+### Fast
+- No title-screen, goes straight to a game (press START to open a menu)
+- Instant resume: no need to "save", just power off
+### Designed for Real Hardware
+- Long battery life; power-down the CPU when idling / no idle animation
+- Stick to 4 MHz on GameBoy Color
+- GameBoy mono (DMG), Super GameBoy & GameBoy Color support and features
--- build.bat
+++ build.bat
@@ -3,9 +3,26 @@ CLS & TITLE Building Because...
 CD %~dp0
 ECHO:
 
+REM # convert the graphics to native format;
+REM # we use "rgbfix" from the RGBDS assembler,
+REM # even though we won't be using RGBDS to assemble
+SET RGBGFX="bin\rgbds\rgbgfx.exe"
+
+%RGBGFX% -v ^
+    -o "build\tiles_even.2bpp" ^
+       "src\gfx\tiles_even.png"
+
+IF ERRORLEVEL 1 EXIT /B 1
+
+%RGBGFX% -v ^
+    -o "build\tiles_odd.2bpp" ^
+       "src\gfx\tiles_odd.png"
+
+IF ERRORLEVEL 1 EXIT /B 1
+
+REM # the assembler and linker is WLA-DX
 SET WLA_GB80="bin\wla-dx\wla-gb.exe"    -x -I "src"
 SET WLA_LINK="bin\wla-dx\wlalink.exe"   -A -S
-SET BGB="bin\bgb\bgb.exe"
 
 %WLA_GB80% -v ^
     -o "build\because.o" ^
@@ -19,4 +36,6 @@ IF ERRORLEVEL 1 EXIT /B 1
 
 IF ERRORLEVEL 1 EXIT /B 1
 
+REM # start the emulator
+SET BGB="bin\bgb\bgb.exe"
 START "" %BGB% "build\because.gb"
